d09f7b8f00655c1a7375456d71c17ab2a8938544
Usernamegrv <139330806+Usernamegrv@users.noreply.github.com>
2024-02-18 16:48:02 +0100

    correction pagination

--- src/containers/Events/index.js
+++ src/containers/Events/index.js
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import EventCard from "../../components/EventCard";
 import Select from "../../components/Select";
 import { useData } from "../../contexts/DataContext";
@@ -13,26 +13,24 @@ const EventList = () => {
   const { data, error } = useData();
   const [type, setType] = useState();
   const [currentPage, setCurrentPage] = useState(1);
-  const filteredEvents = (// ajout .filter à events pour filtrer les événements par type
-    !type ? data?.events : data?.events.filter(event => event.type === type ) 
-    ) || [];
-    filteredEvents.filter((_, index) => {
-    if (
-      (currentPage - 1) * PER_PAGE <= index &&
-      PER_PAGE * currentPage > index
-    ) {
-      return true;
+  const [filteredEvents, setFilteredEvents] = useState([]);
+  // ajout .filter a events +  setFilter + useEffect
+  useEffect(() => {
+    if (data) {
+      const filtered = !type
+        ? data.events
+        : data.events.filter((event) => event.type === type);
+      setFilteredEvents(filtered);
     }
-    return false;
-    });
+  }, [data, type, currentPage]);
   const changeType = (evtType) => {
     setCurrentPage(1);
     setType(evtType);
   };
-  const pageNumber = Math.floor((filteredEvents?.length || 0) / PER_PAGE) + 1;
+  const pageNumber = Math.floor((filteredEvents.length || 0) / PER_PAGE) + 1;
   const typeList = new Set(data?.events.map((event) => event.type));
-  if (error || (data === null &&  !error)) {
-    <div>An error occurred or data is still loading...</div>;
+  if (error || (data === null && !error)) {
+    return <div>An error occurred or data is still loading...</div>;
   }
   return (
     <>
@@ -47,19 +45,25 @@ const EventList = () => {
             onChange={(value) => (value ? changeType(value) : changeType(null))}
           />
           <div id="events" className="ListContainer">
-            {filteredEvents.map((event, index) => (
-              <Modal key={event.id || index} Content={<ModalEvent event={event} />}>
-                {({ setIsOpened }) => (
-                  <EventCard
-                    onClick={() => setIsOpened(true)}
-                    imageSrc={event.cover}
-                    title={event.title}
-                    date={new Date(event.date)}
-                    label={event.type}
-                  />
-                )}
-              </Modal>
-            ))}
+            {filteredEvents
+              .slice((currentPage - 1) * PER_PAGE, currentPage * PER_PAGE)
+              .map((event, index) => (
+                // ajout slice et map pour pagination
+                <Modal
+                  key={event.id || index}
+                  Content={<ModalEvent event={event} />}
+                >
+                  {({ setIsOpened }) => (
+                    <EventCard
+                      onClick={() => setIsOpened(true)}
+                      imageSrc={event.cover}
+                      title={event.title}
+                      date={new Date(event.date)}
+                      label={event.type}
+                    />
+                  )}
+                </Modal>
+              ))}
           </div>
           <div className="Pagination">
             {[...Array(pageNumber || 0)].map((_, n) => (
